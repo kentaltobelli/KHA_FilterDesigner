53cb69ed4787a5a5b68c3906b65b6bac89d40125
kentaltobelli <kent.h.altobelli@uscga.edu>
2014-11-13 12:09:46 -0500

    Attempted digital

--- DispPoles.m
+++ DispPoles.m
@@ -1,6 +1,14 @@
-function DispPoles(n, coef, poles)
+function DispPoles(filter_domain, n, poles)
 
-% Display poles
-for m=1:ceil(n)
-    fprintf('w0 = %.2f, Q = %.4f\n', coef*(abs(poles(m))), 1./(2*cos(angle(poles(m)))));
+if (strcmpi(filter_domain, 'analog'))
+    % Display analog poles
+    for m=1:ceil(n)
+        fprintf('w0 = %.2f, Q = %.4f\n', (abs(poles(m))), 1./(2*cos(angle(poles(m)))));
+    end
+else
+    % Display digital poles
+    for m=1:ceil(n)
+        %fprintf('1 + (%.4f)z^(-1) + (%.4f)z^(-2)\n', -2*real(poles(m)), (abs(poles(m)))^2);
+        fprintf('1  %.4f  %.4f\n', -2*real(poles(m)), (abs(poles(m)))^2);
+    end
 end
---
+++ Prewarp.m
@@ -0,0 +1,3 @@
+function new_freq = Prewarp(old_freq)
+
+new_freq = 2*tan(pi*old_freq);
